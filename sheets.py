ca4a7b800587fed89bcde7ad46d8a09e96dede6d
NickSilva121 <pereira.nicolas1210@gmail.com>
2026-08-31 12:29:54 -0300

    Corrige conexão com Google Sheets

--- sheets.py
+++ sheets.py
@@ -1,23 +1,39 @@
 import os
+
 import streamlit as st
 import gspread
+
 from google.oauth2.service_account import Credentials
+
+
+# ======================================================
+# CONFIGURAÇÃO
+# ======================================================
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
+PLANILHA_ID = "1k0Y_nl8KVuGtgFIssJKZrWOu1nv1Xa74RN8QTsm8PHM"
+
+
+# ======================================================
+# CONEXÃO COM GOOGLE SHEETS
+# ======================================================
 
 def conectar_planilha():
 
     if os.path.exists("tough-plate-354500-7526720d6b28.json"):
+
         # Execução local
         creds = Credentials.from_service_account_file(
             "tough-plate-354500-7526720d6b28.json",
             scopes=SCOPES
         )
+
     else:
+
         # Streamlit Cloud
         creds = Credentials.from_service_account_info(
             dict(st.secrets["gcp_service_account"]),
@@ -26,4 +42,7 @@ def conectar_planilha():
 
     client = gspread.authorize(creds)
 
-    return client.open("Dashboard Importações")
+    # Abre diretamente pela ID
+    planilha = client.open_by_key(PLANILHA_ID)
+
+    return planilha
